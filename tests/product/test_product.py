c22e6ce37fc58f96545879bd94b2b2622c2c09d2
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-05-08 20:27:27 -0300

    feat: Testar o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import datetime
+import pytest
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def sample_product():
+    return Product(
+        id=1,
+        nome_do_produto="Arroz",
+        nome_da_empresa="MarcaFácil",
+        data_de_fabricacao=datetime.now(),
+        data_de_validade=datetime.now().replace(year=datetime.now().year+1),
+        numero_de_serie="123ABC",
+        instrucoes_de_armazenamento="Manter em local seco e arejado",
+    )
+
+
+def test_cria_produto(sample_product):
+    assert isinstance(sample_product.id, int)
+    assert isinstance(sample_product.nome_da_empresa, str)
+    assert isinstance(sample_product.nome_do_produto, str)
+    assert isinstance(sample_product.data_de_fabricacao, str)
+    assert isinstance(sample_product.data_de_validade, str)
+    assert isinstance(sample_product.numero_de_serie, str)
+    assert isinstance(sample_product.instrucoes_de_armazenamento, str)
