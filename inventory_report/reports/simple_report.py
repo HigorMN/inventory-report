adffffc3dce0bf55828a6c673e1222e2ed0fd836
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-05-08 22:52:27 -0300

    Gerar a versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,34 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    def generate(products):
+        # Cria uma lista  que contém todas as datas de fabricação dos
+        # produtos em ordem crescente.
+        dates_fabrication = sorted(
+            [p["data_de_fabricacao"] for p in products]
+        )
+
+        # Encontra a data de validade mais próxima
+        closest_expiration_date = min(
+            [
+                p["data_de_validade"]
+                for p in products
+                if datetime.strptime(p["data_de_validade"], "%Y-%m-%d")
+                > datetime.now()
+            ]
+        )
+
+        # Armazena o nome da empresa que possui a maior quantidade
+        # de produtos na lista
+        company_with_most_products = Counter(
+            [p["nome_da_empresa"] for p in products]
+        ).most_common(1)[0][0]
+
+        # Retorna uma string contendo as informações do relatório simplificado
+        return (
+            f"Data de fabricação mais antiga: {dates_fabrication[0]}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_most_products}"
+        )
