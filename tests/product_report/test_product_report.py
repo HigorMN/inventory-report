9f333d0107c2ab67e64a9c7534a5a0bd7da23e1b
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-05-09 21:44:00 -0300

    feat: Testar o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Core-i913900k",
+        "Kabum",
+        "10-05-2023",
+        "10-05-2030",
+        "ABC123",
+        "em um local seco",
+    )
+    expected_output = (
+        "O produto Core-i913900k fabricado em 10-05-2023 por Kabum "
+        "com validade até 10-05-2030 precisa ser armazenado "
+        "em um local seco."
+    )
+    assert str(product) == expected_output
